3eeff02adfb155ee86f5c5b6d61d20e71910e4b2
jose050204 <josemiguelriscomunoz@gmail.com>
2025-10-27 12:43:58 +0100

    commit index

---
+++ index.php
@@ -0,0 +1,58 @@
+<!-- HOME - Tu Dashboard
+Menú de navegación: 
+🏠 Home | 🎯 Desafío 1 | 🔥 Desafío 2 | ⚡ Desafío 3 | 🏆 Desafío 4 | 💎 Desafío 5
+Contenido:
+Saludo personalizado con tu nombre de usuario gamer (almacenado en sesión)
+"¡Bienvenido de nuevo, [username]! 🎮"
+Muestra 20 avatares/iconos de streamers desde la carpeta /images/streamers/
+Contador de "racha de días": cuántos días consecutivos has entrado (usando cookies)
+Stats en tiempo real:
+Última conexión (guardada en cookie con duración de 30 días)
+Total de visitas a tu dashboard (contador en archivo)
+Requisitos técnicos:
+Al entrar por primera vez, formulario para elegir tu username gamer (guardado en sesión)
+Cookie que registre tu última visita
+Cargar los 20 avatares dinámicamente desde el sistema de archivos -->
+
+
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Indice</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<body>
+
+    
+</body>
+</html>
